e025d44f4e16ede29adcb1a69b0447de064bed66
iremadze2O1 <iremadzei201@gmail.com>
2024-05-27 21:34:24 +0400

    third commit

---
+++ level 5/homework/lesson 5.py
@@ -0,0 +1,99 @@
+
+name = "ilia"
+
+surname = "iremadze"
+age = 13
+
+address = "tbilisi didi dighomi"
+
+hobby = "solving rubiks cubes"
+
+print("hello my name is" , name , surname , "and i am" , age , "years old." , "i live in" , address)
+
+#addition +
+
+print(65 + 88)
+
+print(87 + 89)
+
+print(56 + 87)
+
+print(92 + 45)
+
+print(67 + 13)
+
+print(56 + 38)
+
+print(72 + 34)
+
+print(81 + 64)
+
+print(78 + 37)
+
+print(19 + 67)
+
+#subtraction -
+
+print(562 - 394)
+
+print(547 - 451)
+
+print(894 - 365)
+
+print(764 - 324)
+
+print(624 - 154)
+
+print(456 - 318)
+
+print(984 - 865)
+
+print(321 - 249)
+
+print(651 - 324)
+
+print(518 - 248)
+
+#multiplacation * 
+
+print(7 * 3)
+
+print(3 * 4)
+
+print(8 * 3)
+
+print(19 * 3)
+
+print(16 * 13)
+
+print(4 * 52)
+
+print(9 * 4)
+
+print(5 * 8)
+
+print(65 * 3)
+
+print(54 * 4)
+
+#division //
+
+print(984 // 6)
+
+print(650 // 5)
+
+print(363 // 3)
+
+print(5124 // 6)
+
+print(6528  // 8)
+
+print(8343 // 6)
+
+print(5190 // 10)
+
+print(1989 // 9)
+
+print(8427 // 3)
+
+print(9848 // 2)
