c5c18d9e20b3d6d8046de5d8c2cb95905888e6dc
iremadze2O1 <iremadzei201@gmail.com>
2024-05-27 15:59:02 +0400

    second commit

---
+++ level 3/homework/lesson 3.py
@@ -0,0 +1,20 @@
+#addition +
+print(6571+2687)
+
+#subtraction -
+print(3571-1568)
+
+#devision /
+print(1836/3)
+
+#multipacition *
+print(35*23)
+
+#first variable for subtraction
+saklebi = 5645
+
+#second variable for subtraction
+maklebi = 3716
+
+#subtraction
+print(saklebi-maklebi)
