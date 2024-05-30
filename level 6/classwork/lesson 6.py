bec0f526d847a056da2cf8b57ba3f927c7d6a5eb
iremadze2O1 <iremadzei201@gmail.com>
2024-05-30 22:23:16 +0400

    fourth commit

---
+++ level 6/classwork/lesson 6.py
@@ -0,0 +1,19 @@
+#first class work
+
+number_1 = input()
+
+number_2 = input()
+
+print(int(number_1) + int(number_2))
+
+#second class work
+
+name = input("enter your name: ")
+
+surname = input("enter your surname: ")
+
+age = input("enter your age: ")
+
+email = input("enter your e-mail:" )
+
+print("this is your information" , name , surname , age , email)
---
+++ level 6/homework/lesson6.py
@@ -0,0 +1,67 @@
+#addition +
+
+print("this is addition")
+
+addition_number_1 = input()
+
+addition_number_2 = input()
+
+print(int(addition_number_1) + int(addition_number_2))
+
+#subtraction - 
+
+print("this is subtraction")
+
+subtraction_number_1 = input()
+
+subtraction_number_2 = input()
+
+print(int(subtraction_number_1) - int(subtraction_number_2))
+
+#multiplacation *
+
+print("this is multiplacation")
+
+multiplacation_number_1 = input()
+
+multiplacation_number_2 = input()
+
+print(int(multiplacation_number_1) * int(multiplacation_number_2))
+
+#division /
+
+print("this is division")
+
+division_number_1 = input()
+
+division_number_2 = input()
+
+print(int(division_number_1) / int(division_number_2))
+
+#congratulatior
+
+print("this is to congratuade someone")
+
+name_to_congraduate_someone = input("please enter your name here :")
+
+print("congratulations" , name_to_congraduate_someone)
+
+#for registration
+
+print("this is for registration :")
+
+name_for_registration = input("please enter your name here :")
+
+surname_for_registration = input("please enter your surname here :")
+
+age_for_registration = input("please enter your age here :")
+
+print(name_for_registration , surname_for_registration , age_for_registration)
+
+#to add 25 years to someones age
+
+print("to add 25 years to someones age")
+
+age_to_add_25_years = input("please eneter your age here :")
+
+print(int(age_to_add_25_years) + 25)
