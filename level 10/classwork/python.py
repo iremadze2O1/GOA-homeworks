2b4f28e948fa87751f2bf4bf9fe1e9d3a8ccf298
iremadze2O1 <iremadzei201@gmail.com>
2024-06-13 13:53:48 +0400

    tenth commit

---
+++ level 10/classwork/python.py
@@ -0,0 +1,2 @@
+for i in range(1001):
+    print(str(i) + "." + "i miss raja D:")
---
+++ level 10/homework/lwsson10.py
@@ -0,0 +1,29 @@
+for i in range(11):
+    print(str(i) + "." + "GOA homework")
+
+for i in range(19):
+    print(str(i) + "." + "my dog's name is Babi")
+
+for i in range(21):
+    print(str(i) + "." + "and i love her <3")
+
+for i in range(10):
+    print(str(i) + "." + "idk what to write...")
+
+for i in range(61):
+    print(str(i) + "." + "i miss raja D:")
+
+for i in range(31):
+    print(str(i) + "." + "my countrys name is somiwo")
+
+for i in range(10):
+    print(str(i) + "." + "its near oni")
+
+for i in range(81):
+    print(str(i) + "." + "again... idk what to write...")
+
+for i in range(19):
+    print(str(i) + "." + "im bored...")
+
+for i in range(41):
+    print(str(i) + "." + "i had a concert yesterday :D")
